f42885ac1fb10145e3ac10b2b1d68f8f1809b98d
s40523242 <40523242@gm.nfu.edu.tw>
2020-01-04 02:06:03 +0800

    edit kgtolb

--- downloads/dart_ex/kgtolbhtml/kgtolbhtml.dart
+++ downloads/dart_ex/kgtolbhtml/kgtolbhtml.dart
@@ -1,36 +1,28 @@
 import "dart:html";
-
-lbtokg(num lb){
-return lb*0.4536;
+GtoB(num g){
+return g*2.2046;
 }
-
-kgtolb(num kg){
-return kg*2.2046;
+BtoG(num b){
+return b*0.4535;
 }
-
 main() {
-    
-    InputElement tempInput = querySelector("#weight");
+    InputElement tempInput = querySelector("#temp");
     querySelector("#submit").onClick.listen((e) => convert(tempInput.value));
 }
-
-convert(String data){ 
-  int len;  
-  var type;  
+convert(String data){
+  int len;
+  var type;
   var number;
-  
   LabelElement output = querySelector("#output");
-  
-  len = data.length;  
+  len = data.length;
   type = data[len-2];
   number = data.substring(0, len-2);
-  
   number = int.parse(number);
-  if (type == "L" || type == "l"){
-     output.innerHtml = " $number 英磅 =  ${lbtokg(number).toStringAsFixed(4)} 公斤";
-  } else if ((type == "K" || type == "k")){
-     output.innerHtml = " $number 公斤 =  ${kgtolb(number).toStringAsFixed(4)} 英磅";
+  if (type == "K" || type == "k"){
+     output.innerHtml = "公斤 $number kg = 英磅 ${GtoB(number).toStringAsFixed(4)} lb";
+  } else if ((type == "L" || type == "l")){
+     output.innerHtml = "英磅 $number lb = 公斤 ${BtoG(number).toStringAsFixed(4)} kg";
   } else {
-     output.innerHtml = "請輸入數字加上 LB 或 KG!";
+     output.innerHtml = "請輸入數字加上 KG 或 LB!";
   }
 } 
