d9ebc1aa9e98f81b836d8d451a64447bcbca9bdb
s40523242 <40523242@gm.nfu.edu.tw>
2020-01-09 00:56:25 +0800

    reveal

--- downloads/dart_ex/kgtolbhtml/kgtolbhtml.dart
+++ downloads/dart_ex/kgtolbhtml/kgtolbhtml.dart
@@ -21,7 +21,7 @@ convert(String data){
   if (type == "K" || type == "k"){
      output.innerHtml = " $number kg =  ${GtoB(number).toStringAsFixed(4)} lb";
   } else if ((type == "L" || type == "l")){
-     output.innerHtml = " $number lb = 公斤 ${BtoG(number).toStringAsFixed(4)} kg";
+     output.innerHtml = " $number lb =  ${BtoG(number).toStringAsFixed(4)} kg";
   } else {
      output.innerHtml = "請輸入數字加上 KG 或 LB!";
   }
